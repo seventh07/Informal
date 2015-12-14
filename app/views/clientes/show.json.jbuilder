ed78ce9e4e3871364bb00f8689b30bdc37e86f61
Adrian Dellatorre <adrianld94@gmail.com>
2015-12-14 11:56:58 -0200

    Criando Scaffold para Cliente

---
+++ app/views/clientes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @cliente, :id, :nome, :cpf, :telefone, :created_at, :updated_at
---
+++ spec/factories/clientes.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :cliente do
+    nome "MyString"
+cpf "MyString"
+telefone "MyString"
+  end
+
+end
